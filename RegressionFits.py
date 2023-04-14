0478cf251ec6e768b1f0ba50860d4c58e535b81d
Darrion <darrionatplugins@gmail.com>
2023-04-14 12:21:28 -0400

    Part 3

--- RegressionFits.py
+++ RegressionFits.py
@@ -17,7 +17,7 @@ pylab.rcParams['axes.titlesize'] = 20
 # set font size for labels on axes
 pylab.rcParams['axes.labelsize'] = 20
 # set size of numbers on x-axis
-pylab.rcParams['xtick.labelsize'] = 16
+# pylab.rcParams['xtick.labelsize'] = 16
 # set size of numbers on y-axis
 pylab.rcParams['ytick.labelsize'] = 16
 # set size of ticks on x-axis
@@ -31,24 +31,27 @@ pylab.rcParams['legend.numpoints'] = 1
 
 def getData(fileName, reduced=False):
     dataFile = open(fileName, 'r')
-    distances = []
-    masses = []
+    x = []
+    trials = [[], [], [], []]
     dataFile.readline()
     for line in dataFile:
-        d, m = line.split(' ')
-        distances.append(float(d))
-        masses.append(float(m))
+        values = line.split()
+        x.append(float(values[0]))
+        for i in range(1, len(values)):
+            trials[i - 1].append(float(values[i]))
     dataFile.close()
-    if reduced:
-        amt_remove = 6
-        masses = masses[: len(masses) - 6]
-        distances = distances[: len(distances) - 6]
-    return (masses, distances)
+    # if reduced:
+    #     amt_remove = 6
+    #     y = y[: len(y) - 6]
+    #     x = x[: len(x) - 6]
+    return (x, trials)
 
 
-masses, distances = getData('springData.txt')
-print('masses: ', masses)
-print('distances: ', distances)
+#
+#
+# masses, distances = getData('springData.txt')
+# print('masses: ', masses)
+# print('distances: ', distances)
 
 
 def plotData(inputFile, reduced=False):
@@ -78,36 +81,40 @@ def rSquared(measured, predicted):
     return 1 - estimateError / variability
 
 
-def fitData(x, y, reduced=False):
+def fitData(x, y, xLabel, yLabel, title, reduced=False):
+    x = pylab.array(x)
     y = pylab.array(y)
-    forces = pylab.array(x) * 9.81
-    pylab.plot(forces, y, 'ko',
-               label='Measured displacements')
-    title = 'Foot Length vs Height'
+    print(x, y)
+    pylab.plot(x, y, 'ko', label='Measured displacements')
     if reduced:
         title += ' Reduced'
     pylab.title(title)
-    pylab.xlabel('Foot Length (inches)')
-    pylab.ylabel('Height (inches)')
-    a, b = pylab.polyfit(forces, y, 1)
-    predictedDistances = a * pylab.array(forces) + b
+    pylab.xlabel(xLabel)
+    pylab.ylabel(yLabel)
+    a, b, c = pylab.polyfit(x, y, 2)
+    predicted = a * pylab.array(x) * pylab.array(x) + b * pylab.array(x) + c
     k = 1.0 / a
-    pylab.plot(forces, predictedDistances,
+    pylab.plot(x, predicted,
                label='Displacements predicted by\n'
-                     'linear fit, k = ' + str(round(k, 5)))
+                     'quadratic fit, k = ' + str(round(k, 5)))
     pylab.legend(loc='best')
     # pylab.text(1, .35, f'Line: y={round(a, 3)}x+{round(b, 3)}')
     pylab.savefig(f'{title.replace(" ", "")}.pdf')
     pylab.show()
-    r_squared = rSquared(y, predictedDistances)
-    r = sqrt(r_squared)
-    print('R: ', r)
-    print('R-squared: ', rSquared(y, predictedDistances))
+    # r_squared = rSquared(y, predictedDistances)
+    # r = sqrt(r_squared)
+    # print('R: ', r)
+    # print('R-squared: ', rSquared(y, predictedDistances))
 
 
 if __name__ == '__main__':
-    # fitData('springData.txt', True)
-    foot_length = [8.5, 10, 11, 8, 9.5]
-    height = [64, 67, 71, 61, 67]
-    fitData(foot_length, height)
-    # plotData('springData.txt', True)
+    x, trials = getData('launcherData.txt')
+    for i in range(len(trials)):
+        y = trials[i]
+        fitData(x, y, 'Distance', 'Values', f'Launcher Data Trial {i + 1}')
+# fitData('springData.txt', True)
+
+# foot_length = [8.5, 10, 11, 8, 9.5]
+# height = [64, 67, 71, 61, 67]
+# fitData(foot_length, height)
+# plotData('springData.txt', True)
